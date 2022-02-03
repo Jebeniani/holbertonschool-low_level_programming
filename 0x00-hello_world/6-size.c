6f42f91d8ebb31bc91cec7240a70a9926bf8320d
jebeniani <aymenjebeniani05@gmail.com>
2022-02-03 16:45:59 +0100

    6.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-char *variabletype[] = {"char", "unsigned char", "signed char", "int", "unsigned int", "short", "unsigned short", "long", "unsigned long", "long long", "unsigned long long"};
 
-int main() {
-	for (int i = 0; i < 11;++i) {
-	printf("Size of %s is %u\n",variabletype[i], (unsigned int)(sizeof(variabletype[i])))}
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	char 1;
+	int 2; 
+	long int 3;
+	long long int 4;
+	float 5;
+
+	printf("size of a char: %zu byte(s)\n", sizeof(1);
+	printf("size of an int: %zu byte(s)\n", sizeof(2));
+	printf("size of a long int: %zu byte(s)\n", sizeof(3));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(4));
+	printf("size of a float: %zu byte(s)\n", sizeof(5));
 	return (0);
 }
