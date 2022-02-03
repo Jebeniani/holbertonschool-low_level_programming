796d52121829c422571f6b14b870f3800298508e
jebeniani <aymenjebeniani05@gmail.com>
2022-02-03 16:56:07 +0100

    6.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	long long int R;
 	float T;
 
-	printf("size of a char: %zu byte(s)\n", sizeof(A);
+	printf("size of a char: %zu byte(s)\n", sizeof(A));
 	printf("size of an int: %zu byte(s)\n", sizeof(Z));
 	printf("size of a long int: %zu byte(s)\n", sizeof(E));
 	printf("size of a long long int: %zu byte(s)\n", sizeof(R));
