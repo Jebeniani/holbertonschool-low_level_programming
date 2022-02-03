dbd297e2c9b7e4cd0b1dccff15d689c2acccfe82
jebeniani <aymenjebeniani05@gmail.com>
2022-02-03 13:21:17 +0100

    4.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
-#!/bin/bash
-gcc puts("Programming is like building a multilingual puzzle,)
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+	   char str1[15];
+		       puts("Programming is like building a multilingual puzzle")
+			     
+			 	  return(0);
+}
