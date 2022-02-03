2c1f104b9990585d25ab98cff8d400ae54931fdb
jebeniani <aymenjebeniani05@gmail.com>
2022-02-03 19:36:15 +0100

    101.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,6 +2,6 @@
 int write(int filedes, const char *buf, unsigned int nbyte);
 int main( int argc, char** argv )
 {
-	write(, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 50);
+	write(("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", (50));
 	return (1);
 }
