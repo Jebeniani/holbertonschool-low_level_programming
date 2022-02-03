6a8eb881d7abf7c98d1eacca83ecb98307ad0890
jebeniani <aymenjebeniani05@gmail.com>
2022-02-03 19:34:05 +0100

    101.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,7 @@
+#include <stdio. h>
+int write(int filedes, const char *buf, unsigned int nbyte);
+int main( int argc, char** argv )
+{
+	write(, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 50);
+	return (1);
+}
