a6712edb63a4f64e40338be5556280ffb533c62c
jebeniani <aymenjebeniani05@gmail.com>
2022-02-04 17:49:27 +0100

    3.

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, lower_x, upper_x;
+
+	x = 'A';
+
+	for (x = 'A'; x <= 'Z'; x++)
+{
+	lower_x = tolower(x);
+	putchar (lower_x);
+}
+	putchar (\x)
+	putchar ('\n');
+	return (0);
+}
