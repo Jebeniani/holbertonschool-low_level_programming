1f26cd9288568a29d0c1c7d9afd3300ca8aaa12b
jebeniani <aymenjebeniani05@gmail.com>
2022-02-04 19:29:16 +0100

    7.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'Z'; i >= 'A'; i--)
+{
+	printf("%c", i);
+}
+	return (0);
+}
