dba86531d09b788b075e433b48c902a01b8c8178
jebeniani <aymenjebeniani05@gmail.com>
2022-02-04 16:52:11 +0100

    2.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <ctype.h>
-int main()
+int main(void)
 {
 	int x, lower_x;
-	x = 'A'; 
+	x = 'A';
 
-	for( x = 'A'; x <= 'Z'; x++)
-	{
+	for (x = 'A'; x <= 'Z'; x++)
+{
 	lower_x = tolower(x);
-	putchar(lower_x);
-	}
-	putchar('\n')
-	return 0;
+	putchar (lower_x);
+}
+	putchar ('\n');
+	return (0);
 }
