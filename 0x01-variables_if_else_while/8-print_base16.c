8df8974b9682c716cb9e2079c1844a11955f8cfe
jebeniani <aymenjebeniani05@gmail.com>
2022-02-04 19:52:56 +0100

    8.

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, c;
+
+	x = '0';
+
+	for (x = '0'; x <= '9'; x++)
+{
+	putchar (x);
+}
+	for (c = 'a'; c <= 'f'; c++)
+	putchar (c);
+	putchar (\n);
+}
