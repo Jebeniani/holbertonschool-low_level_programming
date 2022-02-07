956c36da733122a155833b817cd2c0c510dbdabf
jebeniani <aymenjebeniani05@gmail.com>
2022-02-07 19:33:42 +0100

    100.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,12 +12,12 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = '8'; j <= '9'; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
 			if (i < j)
 			{
-				putchar('i');
-				putchar('j');
+				putchar(i);
+				putchar(j);
 
 				if ((i + j) != ('8' + '9'))
 				{
