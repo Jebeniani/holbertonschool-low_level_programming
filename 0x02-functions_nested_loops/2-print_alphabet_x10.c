9e83a895acd3aaee9a96994f5716c08e58b162f6
jebeniani <aymenjebeniani05@gmail.com>
2022-02-08 22:07:13 +0100

    2.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the lowercase alphabet ten times
+ *
+ * Return: Always return void.
+ */
+
+void print_alphabet_x10(void)
+{
+
+		int i;
+		char t;
+
+			for (t = 1; t <= 10; t++)
+			{
+				for (i = 'a'; i <= 'z'; i++)
+				{
+				_putchar(i);
+				}
+			}
+	_putchar('\n');
+}
