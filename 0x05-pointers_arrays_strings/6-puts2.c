a11371c48c18d0b304e7184c15f4478b0d0c4211
jobnah <4412@holbertonschool.com>
2022-02-15 09:19:09 -0800

    task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,16 +6,41 @@
  * Return: Always 0.
  */
 
+
+int _strlen(char *str);
+
 void puts2(char *str)
+{
+	int i, len;
+
+	len = _strlen(str);
+
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
+
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @str: character
+ *
+ * Return: Always i.
+ */
+
+
+
+int _strlen(char *str)
 {
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
-	;
-
-	for (i = 0; i < str[i]; i += 2)
 	{
-		_putchar(str[i]);
+		i = i;
 	}
-	_putchar('\n');
+	return (i);
 }
