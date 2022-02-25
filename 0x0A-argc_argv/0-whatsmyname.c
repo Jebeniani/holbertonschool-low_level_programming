0e97edc4aebba7ce3e8589dc70ce029bb0f6750a
jobnah <4412@holbertonschool.com>
2022-02-25 13:07:35 +0100

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * @argc: integer
+ * @argv: pointer
+ * Return: Always 0 (Success)
+ */
+
 int main(int argc, char *argv[])
 {
 	int i;
-	for (i = 0;i < argc; i++)
+
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s ", argv[i]);
 	}
