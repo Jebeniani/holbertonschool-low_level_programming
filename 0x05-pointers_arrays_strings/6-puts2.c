3498f74cfacbb6d5800a3e7f4b568b1433c6de67
jobnah <4412@holbertonschool.com>
2022-03-06 14:09:28 +0100

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,44 +3,19 @@
 /**
  * puts2 - function that prints every other character of a string
  * @str: string
- * Return: Always 0.
+ * Return: Always void.
  */
-
-
-int _strlen(char *str);
 
 void puts2(char *str)
-{
-	int i, len;
-
-	len = _strlen(str);
-
-
-	for (i = 0; i < len; i += 2)
-	{
-		_putchar(str[i]);
-	}
-
-	_putchar('\n');
-}
-
-
-/**
- * _strlen - function that returns the length of a string.
- * @str: character
- *
- * Return: Always i.
- */
-
-
-
-int _strlen(char *str)
 {
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		i = i;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
-	return (i);
+	_putchar('\n');
 }
