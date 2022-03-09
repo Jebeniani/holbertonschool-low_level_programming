6cfe36c3c96a1d626e78725a4446fdc92168eef2
jobnah <4412@holbertonschool.com>
2022-03-09 20:43:13 +0100

    get op func

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,3 +1,5 @@
+#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
 
@@ -15,12 +17,17 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL}
-	};
+		{NULL, NULL}};
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (ops[i].f);
+		}
 		i++;
+	}
 
-	return (ops[i].f);
+	return (0);
 }
