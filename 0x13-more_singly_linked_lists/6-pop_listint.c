b5528585a58ac11630ded5a7bca69d832964a45c
jobnah <4412@holbertonschool.com>
2022-03-22 21:25:03 +0100

    pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,7 +3,7 @@
 /**
  * pop_listint - function that deletes the head node of a linked list
  * @head: head of a node
- * Return: Always 0 if the list is empty.
+ * Return: Always 0 if the list is empty or n.
  */
 
 int pop_listint(listint_t **head)
@@ -11,7 +11,7 @@ int pop_listint(listint_t **head)
 	listint_t *tmpr = *head;
 	int n;
 
-	if (!head)
+	if (!*head)
 		return (0);
 	n = tmpr->n;
 	*head = (*head)->next;
