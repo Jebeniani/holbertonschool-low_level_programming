943bc8e9f3a1192a2344b448a482460845f67095
jobnah <4412@holbertonschool.com>
2022-03-22 14:18:12 +0100

    free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint -  function that frees a list_t list.
+ * @head: head of a node
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmpr;
+
+	while (head)
+	{
+		tmpr = head;
+		head = head->next;
+		free(tmpr);
+	}
+}
