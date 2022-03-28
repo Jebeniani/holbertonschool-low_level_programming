d15fffe5dc1bc06de40b0a279de272819a3cc9fe
Jebeniani <aymenjebeniani05@gmail.com>
2022-03-28 14:05:58 -0700

    read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,22 +11,24 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, sst;
-	char *buff = malloc(sizeof(char*) * letters);
+	char *buff;
+
+	buff = malloc(sizeof(char *) * letters);
 
 	if (!buff)
-		return(0);
+		return (0);
 
 	if (!filename)
-		return(0);
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return(0);
+		return (0);
 
 	sst = read(fd, buff, letters);
 	write(STDOUT_FILENO, buff, sst);
 
 	free(buff);
 	close(fd);
-	return(sst);
+	return (sst);
 }
